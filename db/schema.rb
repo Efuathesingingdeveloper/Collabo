cc1963f242d7dff07257e66920b0b030c1f419d3
Efuathesingingdeveloper <66095416+Efuathesingingdeveloper@users.noreply.github.com>
2020-12-09 18:32:24 -0500

    added password

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_25_175810) do
+ActiveRecord::Schema.define(version: 2020_12_04_164259) do
 
   create_table "collabos", force: :cascade do |t|
     t.string "kind"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2020_11_25_175810) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
